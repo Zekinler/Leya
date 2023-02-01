5d53ec7a997c28f8e1e1beb58b1f27ac140beaa5
Ganheso Leso <ganhesoleso@gmail.com>
2023-02-01 08:12:32 -0500

    Update .gitignore

--- deleteCommands.js
+++
@@ -1,8 +0,0 @@
-const { REST, Routes } = require('discord.js');
-const { clientId, token } = require('./config.json');
-
-const rest = new REST({ version: '10' }).setToken(token);
-
-rest.put(Routes.applicationCommands(clientId), { body: [] })
-	.then(() => console.log('Successfully deleted all application commands.'))
-	.catch(console.error);
