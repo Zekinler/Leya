c32010576c69753a21e49b544a9951d3fd31b1ce
Ganheso Leso <ganhesoleso@gmail.com>
2023-07-12 03:49:20 -0400

    Remove "remove levupnotifchan" button from settings menu

--- interactions/leveling/guildSettings/changeLevelingSettings.js
+++ interactions/leveling/guildSettings/changeLevelingSettings.js
@@ -1,15 +1,21 @@
 const { ActionRowBuilder, ButtonBuilder, ButtonStyle, PermissionsBitField } = require('discord.js');
+const { GetDatabaseGuilds } = require('../../database.js');
 
 module.exports = {
 	customId: 'changelevelingsettings',
-	async execute(interaction) {
+	async execute(interaction, db) {
 		if (!(interaction.memberPermissions.has(PermissionsBitField.Flags.ManageGuild & PermissionsBitField.Flags.Administrator) || interaction.user.id === '1007207515353776200')) {
 			await interaction.reply({ content: 'You don\'t have permission to do this', ephemeral: true });
 			return;
 		}
 
-		const rowA = new ActionRowBuilder()
-			.addComponents(
+		const databaseGuilds = await GetDatabaseGuilds(db);
+		const guildLevelingSettings = databaseGuilds.get(interaction.guildId).settings.levelingSettings;
+
+		const rowA = new ActionRowBuilder();
+
+		if (guildLevelingSettings.levelUpMessageChannel !== null) {
+			rowA.addComponents(
 				new ButtonBuilder()
 					.setCustomId('changexprate')
 					.setLabel('Change XP Rate')
@@ -31,6 +37,27 @@ module.exports = {
 					.setLabel('Remove Level-Up Message Channel')
 					.setStyle(ButtonStyle.Primary),
 			);
+		}
+		else {
+			rowA.addComponents(
+				new ButtonBuilder()
+					.setCustomId('changexprate')
+					.setLabel('Change XP Rate')
+					.setStyle(ButtonStyle.Primary),
+				new ButtonBuilder()
+					.setCustomId('changelevelupthreshold')
+					.setLabel('Change Level-Up Threshold')
+					.setStyle(ButtonStyle.Primary),
+				new ButtonBuilder()
+					.setCustomId('changelevelupscaling')
+					.setLabel('Change Level-Up Scaling')
+					.setStyle(ButtonStyle.Primary),
+				new ButtonBuilder()
+					.setCustomId('changelevelupmessagechannel')
+					.setLabel('Set Level-Up Message Channel')
+					.setStyle(ButtonStyle.Primary),
+			);
+		}
 
 		const rowB = new ActionRowBuilder()
 			.addComponents(
